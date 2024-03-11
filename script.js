0de49bea3ae8a332f1a7639afbcd5727a9f76302
zashdwaterlaw <163014290+zashdwaterlaw@users.noreply.github.com>
2024-03-11 21:07:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // script.js
 alert("THIS IS AN ENEMY STAND USER");
+// script.js
+//alert("THIS IS AN ENEMY STAND USER");
+let firstName = 'tezash'
+let lastName = 'mishra'
+let age = 17
+letisHappy = true
+
+let userIntro = 'Hi my name is' + ' ' + firstName + ' ' + lastName + ' ' + 'i am ' + age + ' ' + 'years old'
